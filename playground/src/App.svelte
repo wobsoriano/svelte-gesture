3d752b8537c4a5fb5d3e2667cd6945958a729c26
Robert Soriano <sorianorobertc@gmail.com>
2022-06-29 10:30:55 -0700

    update playground readme

--- playground/src/App.svelte
+++ playground/src/App.svelte
@@ -1,65 +1,34 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { spring } from 'svelte/motion';
+  import { drag } from 'svelte-gesture'
+
+  let coords = spring({ x: 0, y: 0 });
+
+  function handler(payload) {
+    const {
+      active,
+      movement: [mx, my]
+    } = payload.detail
+
+    coords.set({
+      x: active ? mx : 0,
+      y: active ? my : 0
+    })
+  }
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
+<div
+  class="box"
+  use:drag
+  on:drag={handler}
+  style="transform: translate({$coords.x}px, {$coords.y}px)"
+></div>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  .box {
+    background: skyblue;
+    height: 100px;
+    width: 100px;
+    touch-action: none;
   }
 </style>
