2bdbf4a6a7431dc483665b02a9fbaa67f3e1b7d3
wobsoriano <sorianorobertc@gmail.com>
2024-02-29 12:43:25 -0800

    copy files

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
-// Reexport your entry components here
+export * from './gestures.js'
+
+export * from '@use-gesture/vanilla'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,50 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { spring } from 'svelte/motion';
+  import { drag } from '$lib/index.js'
+  import type { GestureEvent } from '$lib/index.js'
+  
+  let coords = spring({ x: 0, y: 0, scale: 1 });
+
+  function handler({ detail }: CustomEvent<GestureEvent<'drag'>>) {
+    const {
+      active,
+      movement: [mx, my]
+    } = detail
+
+    coords.set({
+      x: active ? mx : 0,
+      y: active ? my : 0,
+      scale: active ? 1.2 : 1
+    })
+  }
+</script>
+
+<div class="flex fill center">
+  <div
+    class="drag"
+    use:drag
+    on:drag={handler}
+    tabindex="-1"
+    style:transform="translate3d({$coords.x}px, {$coords.y}px, 0) scale({$coords.scale})"
+  />
+</div>
+
+<style>
+.drag {
+  position: absolute;
+  height: 80px;
+  width: 80px;
+  border-radius: 8px;
+  background-color: hotpink;
+  cursor: grab;
+  touch-action: none;
+  -webkit-user-select: none;
+  user-select: none;
+  font-size: 10px;
+}
+
+.drag:focus {
+  border: 2px solid red;
+  cursor: grabbing;
+}
+</style>
